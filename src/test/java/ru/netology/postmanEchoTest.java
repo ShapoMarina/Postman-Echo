7b0e84a40068ce7ae6ad418e043900cc403bc6ed
Marina Shapovalova <snyzinka@gmail.com>
2023-05-26 15:31:23 +0300

    Second commit

--- src/test/java/ru/netology/postmanEchoTest.java
+++ src/test/java/ru/netology/postmanEchoTest.java
@@ -19,7 +19,7 @@ public class postmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
